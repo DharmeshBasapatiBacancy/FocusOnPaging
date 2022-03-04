45ee64f27ce337d33c46662f39195cfbcf9f9008
DharmeshBasapatiBacancy <dharmesh.basapati@bacancy.com>
2022-03-04 17:53:43 +0530

    Kept header on method body itself.

--- app/src/main/java/com/example/focusonpaging/network/service/MyApiService.kt
+++ app/src/main/java/com/example/focusonpaging/network/service/MyApiService.kt
@@ -9,6 +9,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -21,7 +22,8 @@ interface MyApiService {
         @Query("per_page") size: Int
     ): List<Repo>
 
-    @GET("collections/staffapp_product_v1/documents/search")
+    @Headers( "X-TYPESENSE-API-KEY:zBLij8ihv6XHqz6kpSAsokmxmbdBxWuR")
+    @GET("https://0gkh49werov6qtu1p-1.a1.typesense.net/collections/staffapp_product_v1/documents/search")
     suspend fun searchProduct(
         @Query("q") query: String = "*",
         @Query("query_by") queryBy: String = "brand_name",
@@ -32,21 +34,21 @@ interface MyApiService {
     ): Response<ProductSearchResponseBody>
 
     companion object {
-        private const val BASE_URL = "https://0gkh49werov6qtu1p-1.a1.typesense.net/"//https://api.github.com/
+        private const val BASE_URL = "https://api.github.com/"
 
         fun create(): MyApiService {
             val logger = HttpLoggingInterceptor()
-            logger.level = HttpLoggingInterceptor.Level.BODY
+            logger.level = HttpLoggingInterceptor.Level.HEADERS
 
             val client = OkHttpClient.Builder()
                 .addInterceptor(logger)
-                .addInterceptor(
-                    Interceptor { chain ->
-                        val requestBuilder = chain.request().newBuilder()
-                        requestBuilder.addHeader("X-TYPESENSE-API-KEY", "zBLij8ihv6XHqz6kpSAsokmxmbdBxWuR")
-                        return@Interceptor chain.proceed(requestBuilder.build())
-                    }
-                )
+//                .addInterceptor(
+//                    Interceptor { chain ->
+//                        val requestBuilder = chain.request().newBuilder()
+//                        requestBuilder.addHeader("X-TYPESENSE-API-KEY", "zBLij8ihv6XHqz6kpSAsokmxmbdBxWuR")
+//                        return@Interceptor chain.proceed(requestBuilder.build())
+//                    }
+//                )
                 .build()
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
